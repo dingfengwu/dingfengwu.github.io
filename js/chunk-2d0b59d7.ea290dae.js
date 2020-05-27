fd1e1f2b9236ca092e792d8f160d4bff5e7872bf
robert <robert@wu>
2020-05-28 01:57:30 +0800

    publish new version

---
+++ js/chunk-2d0b59d7.ea290dae.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b59d7"],{"1a3d":function(e,s,n){"use strict";n.r(s);var u=function(){var e=this,s=e.$createElement,n=e._self._c||s;return n("div",[e._v("userMessages")])},a=[],t={name:"userMessages"},r=t,c=n("2877"),l=Object(c["a"])(r,u,a,!1,null,null,null);s["default"]=l.exports}}]);
